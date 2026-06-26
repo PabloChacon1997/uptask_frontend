503246c06638212c784ca1a0d4c552de736638fb
Pablo Andres Chacon <pabloandres120297@gmail.com>
2026-06-25 20:47:34 -0500

    initial commit

---
+++ src/router.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AppLayout from "./layouts/AppLayout";
+import DashboardView from "./views/DashboardView";
+
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route>
+          <Route element={<AppLayout />} >
+            <Route path="/" element={<DashboardView />} index />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
