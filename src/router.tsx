34f301287802536423c95e36800decd53913b769
Pablo Andres Chacon <pabloandres120297@gmail.com>
2026-06-29 19:06:01 -0500

    Working on create project

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import AppLayout from "./layouts/AppLayout";
 import DashboardView from "./views/DashboardView";
+import CreateProjectView from "./views/projects/CreateProjectView";
 
 
 export default function Router() {
@@ -10,6 +12,7 @@ export default function Router() {
         <Route>
           <Route element={<AppLayout />} >
             <Route path="/" element={<DashboardView />} index />
+            <Route path="/projects/create" element={<CreateProjectView />}/>
           </Route>
         </Route>
       </Routes>
