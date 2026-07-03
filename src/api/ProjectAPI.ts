8958bd56562684320261cac94bf662bf9b06e864
Pablo Andres Chacon <pabloandres120297@gmail.com>
2026-07-02 21:21:17 -0500

    add edit project

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -1,6 +1,6 @@
 import { isAxiosError } from "axios";
 import api from "../lib/axios";
-import { dashboardprojectSchema, type ProjectFormData } from "../types";
+import { dashboardprojectSchema, type Project, type ProjectFormData } from "../types";
 
 
 export async function createProject(formData: ProjectFormData) {
@@ -23,6 +23,35 @@ export async function getProjects() {
     if (response.success) {
       return response.data;
     }
+  } catch (error) {
+    if(isAxiosError(error) && error.response) {
+      // eslint-disable-next-line preserve-caught-error
+      throw new Error(error.response.data.error)
+    }
+  }
+}
+
+export async function getProjectById(id: Project['id']) {
+  try {
+    const { data } = await api(`/projects/${id}`);
+    return data;
+  } catch (error) {
+    if(isAxiosError(error) && error.response) {
+      // eslint-disable-next-line preserve-caught-error
+      throw new Error(error.response.data.error)
+    }
+  }
+}
+
+type PropjectAPIType = {
+  formData: ProjectFormData,
+  projectId: Project['id']
+}
+
+export async function updateProjectById({formData, projectId}: PropjectAPIType) {
+  try {
+    const { data } = await api.put(`/projects/${projectId}`, formData);
+    return data;
   } catch (error) {
     if(isAxiosError(error) && error.response) {
       // eslint-disable-next-line preserve-caught-error
--- src/router.tsx
+++ src/router.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AppLayout from "./layouts/AppLayout";
 import DashboardView from "./views/DashboardView";
 import CreateProjectView from "./views/projects/CreateProjectView";
+import EditProjectView from "./views/projects/EditProjectView";
 
 
 export default function Router() {
@@ -13,6 +14,7 @@ export default function Router() {
           <Route element={<AppLayout />} >
             <Route path="/" element={<DashboardView />} index />
             <Route path="/projects/create" element={<CreateProjectView />}/>
+            <Route path="/projects/:projectId/edit" element={<EditProjectView />}/>
           </Route>
         </Route>
       </Routes>
